9f650f43c6a5ea26cba969c7284869fbbf395109
xiaowei <920716119@qq.com.com>
2018-04-25 21:23:01 +0800

    重新提交代码

---
+++ code/limitless_wristband_application/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'limitiless_band_application' 
+ * Target:  'limitless_applcation' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ code/limitless_wristband_application/USER/wb_pin_map.h
@@ -0,0 +1,6 @@
+#ifndef _WB_PIN_MAP_H
+#define  _WB_PIN_MAP_H
+
+
+#endif
+
